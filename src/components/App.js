9625a84e3a940b8ede10812f50b280745f7d9232
Ugur Akin <uakin@sfu.ca>
2020-06-04 13:15:57 -0700

    Static skeleton for sidebar/profile completed. Sidebar now conatins a placeholder profile image with login/signup buttons and a listgroup menu.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,34 @@ import 'styles/App.css';
 function App() {
   return (
     <div id="App" className="row no-gutters">
-      <div id="SideNav" className="bg-dark col-2"></div>
+      <div id="SideBar" className="bg-dark col-2 pt-5">
+        <div id="ProfileHeader" className="container py-2 bg-secondary">
+          <img className="img-rounded"
+            src="https://via.placeholder.com/250" alt="" />
+          <div className="row">
+            <div className="col text-center align-middle">
+              <a className="btn-sm btn-block btn-primary text-light" href="#">Log In</a>
+            </div>
+            <div className="col text-center align-middle">
+              <a className="btn-sm btn-block btn-primary text-light">Sign Up</a>
+            </div>
+          </div>
+        </div>
+
+        <div id="SideNav">
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item bg-transparent text-light d-flex align-items-center justify-content-between">
+              Cras justo odio
+              <span className="badge badge-primary badge-pill">14</span>
+            </li>
+            <li className="list-group-item bg-transparent text-light">Cras justo odio</li>
+            <li className="list-group-item bg-transparent text-light">Cras justo odio</li>
+            <li className="list-group-item bg-transparent text-light">Cras justo odio</li>
+            <li className="list-group-item bg-transparent text-light">Cras justo odio</li>
+          </ul>
+        </div>
+
+      </div>
 
       <div id="SiteContent" className="col">
         <div id="ContentHeader" className="row justify-content-center">
