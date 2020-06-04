3e973fe441a58e9655ddcc8c6a1fd6277c9f361c
Ugur Akin <uakin@sfu.ca>
2020-06-04 12:31:37 -0700

    First iteration of content skeleton completed (content header jumbotron + player history accordion).

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,80 @@ import 'styles/App.css';
 
 function App() {
   return (
-    <div id="App" className="row">
-      <div id="SideNav" className="col-2"></div>
-      <div id="Content" className="row col">
-        <div className="col-9 offset-1">
-          <div id="Header">
+    <div id="App" className="row no-gutters">
+      <div id="SideNav" className="bg-dark col-2"></div>
+
+      <div id="SiteContent" className="col">
+        <div id="ContentHeader" className="row justify-content-center">
+          <div className="col-9">
             <div className="jumbotron  jumbotron-fluid my-4">
               <div className="container">
                 <h1 className="display-4 text-center">BTC Doubles Night</h1>
                 <hr className="my-4" />
                 <p className="lead text-center">X time until the registration for next doubles night opens.</p>
                 <p className="lead text-center">
-                  <a className="btn btn-primary btn-lg disabled" href="#" role="button">Register Here!</a>            </p>
+                  <a className="btn btn-primary btn-lg disabled" href="body" role="button">Register Here</a>
+                </p>
               </div>
             </div>
           </div>
         </div>
 
+        <div id="ContentBody">
+          <div id="History" className="container-fluid justify-content-center">
+            <h3 id="HistoryHeader" className="card bg-dark p-2 text-light">
+              + History
+            </h3>
+            <div className="accordion" id="accordionExample">
+              <div className="container-fluid">
+                <div className="card-header" id="headingOne" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  <h4 className="mb-0">
+                    <div className="text-left" >
+                      + Date
+                  </div>
+                  </h4>
+                </div>
+
+                <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+
+                </div>
+              </div>
+
+            </div>
+          </div >
+        </div>
       </div>
     </div >
   );
