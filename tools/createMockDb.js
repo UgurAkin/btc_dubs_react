ebedc26f54131c3d602931b68f188b9ee3a9470c
Ugur Akin <uakin@sfu.ca>
2020-06-22 12:27:40 -0700

    Mock api now watches both mock data and create mock db files. Removed temporary db.json file from repo. Some sample match scores and a sample 6-court result object added to mock db.

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const mockData = require('./mockData');
 
-const { users } = mockData;
-const data = JSON.stringify({ users });
+const data = JSON.stringify(mockData);
 const filepath = path.join(__dirname, 'db.json');
 
 fs.writeFile(filepath, data, function(err) {
